b2ca820cea8f2e052608e6b37cea8512b506832a
ValentinTsonevski <tsonevski99@gmail.com>
2024-03-28 23:21:51 +0200

    add Swagger API documentation and proper error handling

---
+++ src/main/java/com/example/foreign_exchange/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.example.foreign_exchange.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                description = "OpenApi documentation for foreign exchange api",
+                title = "Open API specification Foreign Exchange",
+                version = "1.0"),
+        servers = {
+                @Server(description = "Local ENV",url = "http://localhost:8080")
+        }
+)
+public class OpenApiConfig {
+}
